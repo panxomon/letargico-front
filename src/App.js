067f40831c761d9cdbdca924bc857a7ad4d928f0
panxo mon <francisco.mmfu@gmail.com>
2021-06-04 20:52:06 -0400

    primera publicacion, en mi blog cuantico .... (de algun dia)

--- src/App.js
+++ src/App.js
@@ -15,6 +15,13 @@ function App() {
         >
           by PM
         </a>
+
+       <p>
+         Me acabo de enterar que un website no es una pagina web, es un lienzo.
+         -No solo eso, tiene dimensiones, varias.... 
+         seguire con mi cerveza y leyendo, esto sera un blog pulento, no se cuando jajaja.
+         fecha: 4 junio 2021
+       </p> 
       </header>
     </div>
   );
