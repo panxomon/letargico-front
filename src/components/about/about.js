0f5b93f60655ac348610d1f7e88054d00e30f2ab
panxo mon <francisco.mmfu@gmail.com>
2021-06-19 22:19:14 -0400

    correcion en la medida de pixeles de main.png

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,7 +2,7 @@ export const About = () => {
     return (
         <section className="about">
             <p>Soy usuario de cannabis y desarrollador de software, este es mi viaje personal hacia un proyecto que busca descriminalizar el uso, promover el auto cultivo y generar consciencia como alternativa medicinal, todo esto acompañado de musica, software y fotografia.</p>
-            <div className="circle"></div>
+            {/* <div className="circle"></div> */}
         </section>
     )
 }
